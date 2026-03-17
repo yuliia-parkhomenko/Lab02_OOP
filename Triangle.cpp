9c1e454976eff4dadb9d7503cf848a3a29e3534c
Юлія <yuliia.parkhom@gmail.com>
2026-03-17 13:16:30 +0200

    Updated lab: added Heron method

--- Triangle.cpp
+++ Triangle.cpp
@@ -8,21 +8,51 @@ double crossProduct(const Point& A, const Point& B, const Point& C)
 {
   return (B.x-A.x)*(C.y-A.y)-(B.y-A.y)*(C.x-A.x);
 }
+
+double distance(const Point& p1, const Point& p2)
+{
+    return sqrt((p1.x - p2.x)*(p1.x - p2.x) +
+                (p1.y - p2.y)*(p1.y - p2.y));
+}
+
+double triangleArea(const Point& A, const Point& B, const Point& C)
+{
+    double a = distance(A, B);
+    double b = distance(B, C);
+    double c = distance(C, A);
+
+    double p = (a + b + c) / 2;
+
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
 bool Triangle::isDegenerate() const
 {
   return fabs(crossProduct(A, B, C)) < 1e-9;
 }
+
 bool Triangle::contains(const Point &P) const
 {
     double d1 = crossProduct(A, B, P);
     double d2 = crossProduct(B, C, P);
     double d3 = crossProduct(C, A, P);
 
-if ((d1 >= 0 && d2 >= 0 && d3 >= 0) ||
-   (d1 <= 0 && d2 <= 0 && d3 <= 0))
-   return true;
-else
-   return false;
+    if ((d1 >= 0 && d2 >= 0 && d3 >= 0) ||
+        (d1 <= 0 && d2 <= 0 && d3 <= 0))
+        return true;
+    else
+        return false;
+}
+
+bool Triangle::containsHeron(const Point &P) const
+{
+    double S  = triangleArea(A, B, C);
+
+    double S1 = triangleArea(A, B, P);
+    double S2 = triangleArea(B, C, P);
+    double S3 = triangleArea(C, A, P);
+
+    return fabs(S - (S1 + S2 + S3)) < 1e-9;
 }
 
 void runProgram()
@@ -39,35 +69,43 @@ void runProgram()
 
    cout << "C (x, y): ";
    cin >> t.C.x >> t.C.y;
-if (t.isDegenerate())
-{
-   cout << "Трикутник вироджений (площа дорівнює 0)." << endl;
-   return;
-}
-Point P;
 
-while(true)
-{
-   cout <<"Введіть координати точки (x, y): ";
-   cin >> P.x >> P.y;
-  if(P.x ==  0 && P.y == 0)
-   break;
+   if (t.isDegenerate())
+   {
+       cout << "Трикутник вироджений (площа дорівнює 0)." << endl;
+       return;
+   }
 
-   double d1 = crossProduct(t.A, t.B, P);
-   double d2 = crossProduct(t.B, t.C, P);
-   double d3 = crossProduct(t.C, t.A, P);
+   Point P;
 
-  if(fabs(d1) < 1e-9 || fabs(d2) < 1e-9 || fabs(d3) < 1e-9)
-  {
-   cout <<"Точка знаходиться на межі трикутника." << endl;
-  }
+   while(true)
+   {
+       cout <<"Введіть координати точки (x, y): ";
+       cin >> P.x >> P.y;
 
-  else if(t.contains(P))
-    cout <<"Точка знаходиться всередині трикутника." << endl;
+       if(P.x == 0 && P.y == 0)
+           break;
 
- else
- {
-    cout <<"Точка знаходиться поза трикутником." << endl;
-    }
-  }
+       double d1 = crossProduct(t.A, t.B, P);
+       double d2 = crossProduct(t.B, t.C, P);
+       double d3 = crossProduct(t.C, t.A, P);
+
+       if(fabs(d1) < 1e-9 || fabs(d2) < 1e-9 || fabs(d3) < 1e-9)
+       {
+           cout <<"Точка знаходиться на межі трикутника." << endl;
+       }
+       else if(t.contains(P))
+       {
+           cout <<"Точка знаходиться всередині трикутника." << endl;
+       }
+       else
+       {
+           cout <<"Точка знаходиться поза трикутником." << endl;
+       }
+
+       if(t.containsHeron(P))
+           cout << "(Метод Герона: всередині або на межі)" << endl;
+       else
+           cout << "(Метод Герона: поза)" << endl;
+   }
 }
--- Triangle.h
+++ Triangle.h
@@ -4,12 +4,20 @@
 struct Point {
    double x, y;
 };
+
 struct Triangle {
     Point A, B, C;
+
     bool contains(const Point &P) const;
+    bool containsHeron(const Point &P) const; 
     bool isDegenerate() const;
 };
+
 double crossProduct(const Point& A, const Point& B, const Point& C);
+
+double distance(const Point& p1, const Point& p2);
+double triangleArea(const Point& A, const Point& B, const Point& C);
+
 void runProgram();
- 
+
 #endif
